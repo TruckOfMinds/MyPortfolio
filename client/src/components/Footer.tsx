c080fde255a7c8f28a49fcf135f7afd9d0f29244
RoobnAccessCreative <reuben.dubois.000@accesscreative.ac.uk>
2025-07-08 20:00:54 +0100

    chore: began work on the footer component fix: typed the App component to JSX.Element style: changed teh speed and delay of text transisitons in the navbar

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import "./style/Footer.css";
+
+export default function Footer() {
+  return (
+    <section>
+      <footer
+        className="footer
+    w-full h-3/4"
+      ></footer>
+    </section>
+  );
+}
