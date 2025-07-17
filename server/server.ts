0b345fe434388eeeb4a877fc771465964aa3f6ac
RoobnAccessCreative <reuben.dubois.000@accesscreative.ac.uk>
2025-07-17 23:40:24 +0100

    chore: light server work

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-import pg from "pg";
+import { db } from "./db";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors<Request>());
 
 app.listen(4060, () => console.log("hey all, port 4060 here"));
+
 app.get("/api", (_: Request, res: Response) => res.json({ status: "200" }));
+
+app.get("/api/designs", (_: Request, res: Response) => {});
