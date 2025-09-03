df963efd7553c07f8396ac9673bc707a63ccd9c9
RoobnAccessCreative <reuben.dubois.000@accesscreative.ac.uk>
2025-09-03 13:39:40 +0100

    chore: boilerplate for Code.tsx

--- client/src/lib/data.ts
+++ client/src/lib/data.ts
@@ -3,3 +3,8 @@ export const bgTexts: { [k: string]: string } = {
   two: "you've reached two",
   three: "congrats on the hatrick",
 };
+
+export const randomColour = (): "sky" | "pink" | "yellow" => {
+  const choice = Math.floor(Math.random() * 3);
+  return choice === 0 ? "sky" : choice === 1 ? "pink" : "yellow";
+};
--- client/src/pages/Code.tsx
+++ client/src/pages/Code.tsx
@@ -1,11 +1,44 @@
+import type { JSX } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { fetchCodeCards } from "@/utils/serverPortal";
+import { randomColour } from "@/lib/data";
+
+import Header from "@/components/Header";
+import Grid from "@/components/Grid";
+import Card from "@/components/Card";
+
 import "./style/Code.css";
 
 export default function CodePage() {
   return (
     <>
-      <section className="bg-blue-400 test">
-        <p>code</p>
-      </section>
+      <title>RD Portfolio</title>
+      <main>
+        <Grid id="top" className="max-h-fit">
+          <Header variant="Dev" />
+          <Projects />
+        </Grid>
+      </main>
     </>
   );
 }
+
+const Projects = (): JSX.Element => {
+  // ? handle query strings
+
+  const { isPending, isError, error, data } = useQuery({
+    queryKey: ["code"],
+    queryFn: fetchCodeCards,
+  });
+
+  if (isPending) return <></>;
+  if (isError) return <>{error}</>;
+
+  return (
+    <section className="in-grid full max-h-fit">
+      {data.map((d) => (
+        <Card variant="code" codeData={d} colour={randomColour()} />
+      ))}
+    </section>
+  );
+};
